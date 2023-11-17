e66bb4409e0f755abf2d4a4304132fa23fa54327
unknown <paledirefilwe@gmail.com>
2023-11-17 11:51:46 +0200

    request pop up alert to admin

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,11 @@
 const mysql = require("mysql");
 
 module.exports = () => {
-// return mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password:'',
-//     database: 'techservices',
-//     port:3306
-// });
 return mysql.createConnection({
-    host: 'sql8.freemysqlhosting.net',
-    user: 'sql8649877',
-    password:'W2qpPzisg4',
-    database: 'sql8649877',
+    host: 'localhost',
+    user: 'root',
+    password:'',
+    database: 'techservices',
     port:3306
 });
 }
