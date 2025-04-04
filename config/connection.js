1a4140f2eac199d877fb7738bdeb848c987b6552
ICEP-DEV <89523262+ICEP-DEV@users.noreply.github.com>
2025-04-04 14:37:52 +0200

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,21 @@
-const mysql = require("mysql2");
-
-module.exports = () => {
-    return mysql.createConnection({
-        host: 'sql8.freemysqlhosting.net',
-        user: 'sql8649877',
-        password: 'W2qpPzisg4',
-        database: 'sql8649877',
-        port: 3306
-    });
-};
+var mysql = require('mysql2')
+ 
+var connection = mysql.createConnection({
+    host: 'sql8.freesqldatabase.com',
+    user: ' sql8771272',
+    password:'1glfXvXzf1',
+    database: ' sql8771272',
+    port:3306
+});
+connection.connect((err) => {
+ 
+    if (!err)
+ 
+      console.log('Connection Established Successfully')
+    else
+ 
+        console.log('Connection Failed!' + JSON.stringify(err, undefined, 2));
+ 
+});
+ 
+module.exports = connection;
